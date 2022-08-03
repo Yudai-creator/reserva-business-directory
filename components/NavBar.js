4c9eb3bcb331e7a645ebaaaf6bc37ffbaef8035d
Yudai-creator <perezheber73@gmail.com>
2022-08-03 09:49:43 -0400

    custom fonts added

---
+++ components/NavBar.js
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return ( 
+        <div className="bg-gray-900 h-16 flex justify-center items-center">
+            <p className="text-cyan-50">Navbar</p>
+        </div>
+     );
+}
+ 
+export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import styles from '../styles/Home.module.css'
 
+import NavBar from '../components/NavBar'
+
 export default function Home() {
   return (
-    <div className="text-3xl text-green-800 p-2">
-      <p>Hello from Next + Tailwind</p>
+    <div className="home__container">
+      <NavBar/>
     </div>
   )
 }
+
+
