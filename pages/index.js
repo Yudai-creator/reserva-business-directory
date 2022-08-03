0e73d6858f7e8cfc192323fe9bb0c25b3e14d575
Yudai-creator <perezheber73@gmail.com>
2022-08-03 08:46:32 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className='text-3xl text-green-800 p-2'>
+      What's up from Next + Tailwind
+    </div>
+  )
+}
