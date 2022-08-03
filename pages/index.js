18a21d9d9855ff3e276badbd195e72538f0d7e09
Yudai-creator <perezheber73@gmail.com>
2022-08-03 09:04:06 -0400

    eslint fix

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className='text-3xl text-green-800 p-2'>
+    <div className="text-3xl text-green-800 p-2">
       What's up from Next + Tailwind
     </div>
   )
