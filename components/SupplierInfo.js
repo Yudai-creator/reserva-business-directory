259d3ff372e35f2a888eb7b892ddb2dd0911be25
Yudai-creator <perezheber73@gmail.com>
2022-08-04 15:26:58 -0400

    style: adding company logos

--- components/SupplierInfo.js
+++ components/SupplierInfo.js
@@ -4,16 +4,19 @@ const SupplierInfo = ({data}) => {
 
     return ( 
         <div className="text-black">
-          <div className="supplier__list_left-column">
-            <div>
-                <h2 className="font-bold">{data.company_name}</h2>
-                <strong className="text-xs">{data.location} {data.business_type}</strong>
-            </div>  
-            
-            <p className="text-xs">{data.overview}</p>
-            <div className="card__controls">
+            <div className="supplier__list_left-column">
+                <div className="flex justify-between items-center">
+                    <div>
+                        <h2 className="font-bold">{data.company_name}</h2>
+                        <strong className="text-xs">{data.location} {data.business_type}</strong>
+                    </div>
+                    <img src={data.logo} alt="company logo" />
+                </div>  
                 
-            </div>
+                <p className="text-xs">{data.overview}</p>
+                <div className="card__controls">
+                    
+                </div>
             </div>
           {/* <strong>{data.supplier_commodities.product}</strong> */}
         </div>
