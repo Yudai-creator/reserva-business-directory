001668f8712ad9c0488caeb23cd6a4b024673775
Yudai-creator <yudaipx77@gmail.com>
2022-08-10 15:21:32 -0700

    feat:nav links animation

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,20 +7,20 @@ const NavBar = ({children}) => {
         <>
         <div className="navbar__container relative bg-blue-500 p-3 flex flex-col justify-between">
             
-            <div className="navbar-features flex flex-row justify-between items-center mb-3">
+            <div className="navbar-features flex flex-row justify-between items-center pt-2 pb-2">
                 <div className="flex justify-center items-center">
                     <img className="cursor-pointer md:hidden" src="https://ik.imagekit.io/u33i3sss0/Reserva_Business_Directory/hamburger-menu-svg_Wxq0wSF-6.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1659533793921" alt="hamburger manu" />
                     <img className="w-28 hidden md:inline" src="https://ik.imagekit.io/u33i3sss0/Reserva_Business_Directory/reserva-fs-logo_j23muE1bz.png?ik-sdk-version=javascript-1.4.3&updatedAt=1659533612929" alt="reserva fs logo" />
 
                     <div className="hidden md:inline">
-                        <ul className="flex flex-row justify-between items-center mr-4 ml-4">
-                            <li className="mr-3 transition-all ease-in-out duration-200 hover:border-b-2 hover:border-b-white">
+                        <ul className="flex flex-row justify-between items-center ml-14">
+                            <li className="mr-10 relative after:absolute after:top-5 after:right-0 after:w-full after:bg-blue-200 after:h-0.5 after:rounded after:opacity-0 hover:after:opacity-100 after:transition-all after:duration-300 after:translate-y-2 hover:after:translate-y-0">
                                 <Link href="/">Home</Link>
                             </li>
-                            <li className="mr-3 transition-all ease-in-out duration-200 hover:border-b-2 hover:border-b-white">
+                            <li className="mr-10 relative after:absolute after:top-5 after:right-0 after:w-full after:bg-blue-200 after:h-0.5 after:rounded after:opacity-0 hover:after:opacity-100 after:transition-all after:duration-300 after:translate-y-2 hover:after:translate-y-0">
                                 <Link href="/browse-suppliers">Browse Suppliers</Link>
                             </li>
-                            <li className="mr-3 transition-all ease-in-out duration-200 hover:border-b-2 hover:border-b-white">
+                            <li className="mr-10 relative after:absolute after:top-5 after:right-0 after:w-full after:bg-blue-200 after:h-0.5 after:rounded after:opacity-0 hover:after:opacity-100 after:transition-all after:duration-300 after:translate-y-2 hover:after:translate-y-0">
                                 <Link href="/browse-suppliers">Your Profile</Link>
                             </li>
                         </ul>
